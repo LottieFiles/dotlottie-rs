96a30f9237db6c0858084f2bfdb872f6f372e676
samuelOsborne <samu.osborne@gmail.com>
2023-12-11 17:51:44 +0100

    feat: added set frame func, getDuration func

--- dotlottie-rs/src/dotlottie_player.rs
+++ dotlottie-rs/src/dotlottie_player.rs
@@ -6,7 +6,7 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 use std::ffi::CString;
 use std::sync::atomic::AtomicI8;
-use std::sync::{Arc, RwLock, Mutex};
+use std::sync::{Arc, Mutex, RwLock};
 
 #[allow(dead_code)]
 pub struct DotLottiePlayer {
@@ -17,9 +17,9 @@ pub struct DotLottiePlayer {
     direction: Arc<RwLock<AtomicI8>>,
 
     // Animation information related
-    duration: f32,
     current_frame: Arc<RwLock<f32>>,
     total_frames: Arc<RwLock<f32>>,
+    duration: Arc<RwLock<f32>>,
 
     // Data
     animation: Arc<RwLock<*mut Tvg_Animation>>,
@@ -34,10 +34,10 @@ impl DotLottiePlayer {
             loop_animation: false,
             speed: 1,
             direction: Arc::new(RwLock::new(AtomicI8::new(1))),
-            duration: 0.0,
             current_frame: Arc::new(RwLock::new(0.0)),
 
             total_frames: Arc::new(RwLock::new(0.0)),
+            duration: Arc::new(RwLock::new(0.0)),
             animation: Arc::new(RwLock::new(std::ptr::null_mut())),
             canvas: Arc::new(RwLock::new(std::ptr::null_mut())),
             buffer: Mutex::new(vec![]),
@@ -86,12 +86,36 @@ impl DotLottiePlayer {
         };
     }
 
+    pub fn frame(&self, no: f32) {
+        unsafe {
+            let current_frame = &mut *self.current_frame.write().unwrap();
+            let animation = self.animation.read().unwrap().as_mut().unwrap();
+            let canvas = self.canvas.read().unwrap().as_mut().unwrap();
+
+            *current_frame = no;
+
+            tvg_canvas_clear(canvas, false, true);
+
+            tvg_animation_set_frame(animation, *current_frame);
+
+            tvg_canvas_update_paint(canvas, tvg_animation_get_picture(animation));
+
+            tvg_canvas_draw(canvas);
+
+            tvg_canvas_sync(canvas);
+        }
+    }
+
     pub fn get_total_frame(&self) -> f32 {
-        return  self.total_frames.read().unwrap().clone();
+        return self.total_frames.read().unwrap().clone();
+    }
+
+    pub fn get_duration(&self) -> f32 {
+        return self.duration.read().unwrap().clone();
     }
 
     pub fn get_current_frame(&self) -> f32 {
-        return  self.current_frame.read().unwrap().clone();
+        return self.current_frame.read().unwrap().clone();
     }
 
     pub fn get_buffer(&self) -> i64 {
@@ -103,6 +127,86 @@ impl DotLottiePlayer {
         let buffer_lock = self.buffer.lock().unwrap();
 
         buffer_lock.len() as i64
+    }
+
+    pub fn clear(&self) {
+        unsafe {
+            let canvas = self.canvas.read().unwrap().as_mut().unwrap();
+
+            tvg_canvas_clear(canvas, false, true);
+        }
+    }
+
+    pub fn load_animation_from_path(&self, path: &str, width: u32, height: u32) {
+        unsafe {
+            tvg_engine_init(Tvg_Engine_TVG_ENGINE_SW, 0);
+
+            *self.canvas.write().unwrap() = tvg_swcanvas_create();
+
+            let canvas = self.canvas.read().unwrap().as_mut().unwrap();
+
+            let mut buffer_lock = self.buffer.lock().unwrap();
+
+            *buffer_lock = vec![0; (width * height * 4) as usize];
+
+            // self.buffer.as = vec![width * height];
+
+            tvg_swcanvas_set_target(
+                canvas,
+                buffer_lock.as_ptr() as *mut u32,
+                width,
+                width,
+                height,
+                Tvg_Colorspace_TVG_COLORSPACE_ABGR8888,
+            );
+
+            *self.animation.write().unwrap() = tvg_animation_new();
+
+            let animation = self.animation.read().unwrap().as_mut().unwrap();
+
+            let frame_image = tvg_animation_get_picture(animation);
+
+            let load_result =
+                tvg_picture_load(frame_image, path.as_ptr() as *const std::os::raw::c_char);
+
+            if load_result != Tvg_Result_TVG_RESULT_SUCCESS {
+                tvg_animation_del(animation);
+
+                // DotLottieError::LoadContentError;
+            } else {
+                println!("Animation loaded successfully");
+                let total_frames = &mut *self.total_frames.write().unwrap();
+                let duration = &mut *self.duration.write().unwrap();
+                let mut pw: f32 = 0.0;
+                let mut ph: f32 = 0.0;
+                let scale: f32;
+                let mut shiftY: f32 = 0.0;
+                let mut shiftX: f32 = 0.0;
+
+                tvg_picture_get_size(frame_image, &mut pw as *mut f32, &mut ph as *mut f32);
+
+                if pw > ph {
+                    scale = width as f32 / pw;
+                    shiftY = (height as f32 / ph * scale) * 0.5;
+                } else {
+                    scale = height as f32 / ph;
+                    shiftX = (width as f32 - pw * scale) * 0.5;
+                }
+
+                tvg_paint_scale(frame_image, scale);
+                tvg_paint_translate(frame_image, shiftX, shiftY);
+
+                tvg_animation_get_total_frame(animation, total_frames as *mut f32);
+                tvg_animation_get_duration(animation, duration as *mut f32);
+                tvg_animation_set_frame(animation, 0.0);
+                tvg_canvas_push(canvas, frame_image);
+                tvg_canvas_draw(canvas);
+                tvg_canvas_sync(canvas);
+
+                println!("Total frames: {}", *total_frames);
+                println!("Duration: {}", *duration);
+            }
+        }
     }
 
     pub fn load_animation(&self, animation_data: &str, width: u32, height: u32) {
@@ -151,6 +255,7 @@ impl DotLottiePlayer {
             } else {
                 println!("Animation loaded successfully");
                 let total_frames = &mut *self.total_frames.write().unwrap();
+                let duration = &mut *self.duration.write().unwrap();
                 let mut pw: f32 = 0.0;
                 let mut ph: f32 = 0.0;
                 let scale: f32;
@@ -171,14 +276,14 @@ impl DotLottiePlayer {
                 tvg_paint_translate(frame_image, shiftX, shiftY);
 
                 tvg_animation_get_total_frame(animation, total_frames as *mut f32);
-                // tvg_animation_get_duration(animation, &mut self.duration);
+                tvg_animation_get_duration(animation, duration as *mut f32);
                 tvg_animation_set_frame(animation, 0.0);
                 tvg_canvas_push(canvas, frame_image);
                 tvg_canvas_draw(canvas);
                 tvg_canvas_sync(canvas);
 
                 println!("Total frames: {}", *total_frames);
-                println!("Duration: {}", self.duration);
+                println!("Duration: {}", *duration);
             }
         }
     }

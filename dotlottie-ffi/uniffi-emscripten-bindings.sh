60b11d777e58fdf34105379249928f9b69ce0b94
Muhmud Ahmad <muhmud@users.noreply.github.com>
2024-01-09 13:12:36 +0300

    small cleanup for manual specification of emscripten bindings (#22)

--- dotlottie-ffi/uniffi-emscripten-bindings.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
-
-UNIFFI_BINDINGS_CPP_DIR="${UNIFFI_BINDINGS_CPP_DIR:-${SCRIPT_DIR}/uniffi-bindings/cpp}"
-EMSCRIPTEN_BINDINGS_CPP="${EMSCRIPTEN_BINDINGS_CPP:-${SCRIPT_DIR}/emscripten_bindings.cpp}"
-
-# Copy the emscripten bindings to the uniffi cpp bindings directory
-cp "${SCRIPT_DIR}/emscripten_bindings.cpp" "${UNIFFI_BINDINGS_CPP_DIR}/"

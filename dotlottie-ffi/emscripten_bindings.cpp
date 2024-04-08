91827f10afec1765475deaec91a207c8067bc522
Abdelrahman Ashraf <a.theashraf@gmail.com>
2024-04-08 15:03:04 +0700

    feat: 🎸 create_default_config ffi

--- dotlottie-ffi/emscripten_bindings.cpp
+++ dotlottie-ffi/emscripten_bindings.cpp
@@ -64,6 +64,8 @@ EMSCRIPTEN_BINDINGS(DotLottiePlayer)
         .field("layout", &Config::layout)
         .field("marker", &Config::marker);
 
+    function("createDefaultConfig", &create_default_config);
+
     // value_object<ManifestTheme>("ManifestTheme")
     //     .field("id", &ManifestTheme::id)
     //     .field("animations", &ManifestTheme::animations);
@@ -105,7 +107,7 @@ EMSCRIPTEN_BINDINGS(DotLottiePlayer)
     //     .function("onStop", &Observer::on_stop);
 
     class_<DotLottiePlayer>("DotLottiePlayer")
-        .smart_ptr<std::shared_ptr<DotLottiePlayer>>("DotLottiePlayer")
+        .smart_ptr<std::shared_ptr<DotLottiePlayer> >("DotLottiePlayer")
         .constructor(&DotLottiePlayer::init, allow_raw_pointers())
         .function("buffer", &buffer)
         .function("clear", &DotLottiePlayer::clear)

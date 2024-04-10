62246b640c07eb87078d937174c8724c65aa1a7a
Gan Jun Kai <junkai@hey.com>
2024-04-10 20:52:17 +0800

    test: add test for load_animation_path

--- dotlottie-rs/tests/theming.rs
+++ dotlottie-rs/tests/theming.rs
@@ -8,6 +8,7 @@ mod tests {
     use std::{
         fs::{self, File},
         io::Read,
+        path::Path,
     };
 
     use super::*;
@@ -113,6 +114,31 @@ mod tests {
         let string = String::from_utf8(buffer.clone()).unwrap();
 
         assert!(player.load_animation_data(&string, WIDTH, HEIGHT));
+        assert!(player.active_theme_id().is_empty());
+
+        assert!(player.is_playing());
+    }
+
+    #[test]
+    fn test_clear_active_theme_id_after_new_animation_path_is_loaded() {
+        let player = DotLottiePlayer::new(Config {
+            autoplay: true,
+            ..Config::default()
+        });
+
+        let valid_theme_id = "test_theme";
+
+        assert!(
+            !player.load_theme(valid_theme_id),
+            "Expected theme to not load"
+        );
+
+        assert!(player.load_dotlottie_data(include_bytes!("assets/test.lottie"), WIDTH, HEIGHT));
+
+        assert!(player.load_theme(valid_theme_id), "Expected theme to load");
+        assert_eq!(player.active_theme_id(), valid_theme_id);
+
+        assert!(player.load_animation_path("tests/assets/test.json", WIDTH, HEIGHT));
         assert!(player.active_theme_id().is_empty());
 
         assert!(player.is_playing());

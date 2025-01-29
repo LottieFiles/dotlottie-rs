849b36d54df7cde4d86eb10e59f73546658f2e5a
samuelOsborne <samu.osborne@gmail.com>
2025-01-29 13:11:38 +0100

    feat: updated emscripten to module

--- dotlottie-ffi/emscripten_library.js
+++ dotlottie-ffi/emscripten_library.js
@@ -4,30 +4,48 @@ mergeInto(LibraryManager.library, {
     return Date.now();
   },
   observer_on_load(dotlottie_instance_id) {
-    _DOTLOTTIE_BRIDGE_JS.observer_on_load(dotlottie_instance_id);
+    if (Module.dotlottieBridge && Module.dotlottieBridge.observer_on_load) {
+      Module.dotlottieBridge.observer_on_load(dotlottie_instance_id);
+    }
   },
   observer_on_load_error(dotlottie_instance_id) {
-    _DOTLOTTIE_BRIDGE_JS.observer_on_load_error(dotlottie_instance_id);
+    if (Module.dotlottieBridge && Module.dotlottieBridge.observer_on_load_error) {
+      Module.dotlottieBridge.observer_on_load_error(dotlottie_instance_id);
+    }
   },
   observer_on_play(dotlottie_instance_id) {
-    _DOTLOTTIE_BRIDGE_JS.observer_on_play(dotlottie_instance_id);
+    if (Module.dotlottieBridge && Module.dotlottieBridge.observer_on_load_error) {
+      Module.dotlottieBridge.observer_on_play(dotlottie_instance_id);
+    }
   },
   observer_on_pause(dotlottie_instance_id) {
-    _DOTLOTTIE_BRIDGE_JS.observer_on_pause(dotlottie_instance_id);
+    if (Module.dotlottieBridge && Module.dotlottieBridge.observer_on_load_error) {
+      Module.dotlottieBridge.observer_on_pause(dotlottie_instance_id);
+    }
   },
   observer_on_stop(dotlottie_instance_id) {
-    _DOTLOTTIE_BRIDGE_JS.observer_on_stop(dotlottie_instance_id);
+    if (Module.dotlottieBridge && Module.dotlottieBridge.observer_on_load_error) {
+      Module.dotlottieBridge.observer_on_stop(dotlottie_instance_id);
+    }
   },
   observer_on_frame(dotlottie_instance_id, frame_no) {
-    _DOTLOTTIE_BRIDGE_JS.observer_on_frame(dotlottie_instance_id, frame_no);
+    if (Module.dotlottieBridge && Module.dotlottieBridge.observer_on_load_error) {
+      Module.dotlottieBridge.observer_on_frame(dotlottie_instance_id, frame_no);
+    }
   },
   observer_on_render(dotlottie_instance_id, frame_no) {
-    _DOTLOTTIE_BRIDGE_JS.observer_on_render(dotlottie_instance_id, frame_no);
+    if (Module.dotlottieBridge && Module.dotlottieBridge.observer_on_load_error) {
+      Module.dotlottieBridge.observer_on_render(dotlottie_instance_id, frame_no);
+    }
   },
   observer_on_loop(dotlottie_instance_id, loop_count) {
-    _DOTLOTTIE_BRIDGE_JS.observer_on_loop(dotlottie_instance_id, loop_count);
+    if (Module.dotlottieBridge && Module.dotlottieBridge.observer_on_load_error) {
+      Module.dotlottieBridge.observer_on_loop(dotlottie_instance_id, loop_count);
+    }
   },
   observer_on_complete(dotlottie_instance_id) {
-    _DOTLOTTIE_BRIDGE_JS.observer_on_complete(dotlottie_instance_id);
+    if (Module.dotlottieBridge && Module.dotlottieBridge.observer_on_load_error) {
+      Module.dotlottieBridge.observer_on_complete(dotlottie_instance_id);
+    }
   },
 });

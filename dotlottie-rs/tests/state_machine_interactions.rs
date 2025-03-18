43ba947a041a5425019aa0504beaa0c565255442
samuelOsborne <samu.osborne@gmail.com>
2025-03-18 15:00:42 +0100

    chore: merged with main, blocked intersect and layer_bound behind feature flag

--- dotlottie-rs/tests/state_machine_interactions.rs
+++ dotlottie-rs/tests/state_machine_interactions.rs
@@ -43,44 +43,73 @@ mod tests {
 
     #[test]
     pub fn pointer_down_up_test() {
-        let global_state =
-            include_str!("fixtures/statemachines/interaction_tests/pointer_down_up.json");
-        let player = DotLottiePlayer::new(Config::default());
-        player.load_dotlottie_data(include_bytes!("fixtures/star_marked.lottie"), 100, 100);
-        let l = player.state_machine_load_data(global_state);
+        #[cfg(feature = "thorvg-v1")]
+        {
+            let global_state =
+                include_str!("fixtures/statemachines/interaction_tests/pointer_down_up.json");
+            let player = DotLottiePlayer::new(Config::default());
+            player.load_dotlottie_data(include_bytes!("fixtures/star_marked.lottie"), 100, 100);
+            let l = player.state_machine_load_data(global_state);
 
-        let s = player.state_machine_start(OpenUrl::default());
+            let s = player.state_machine_start(OpenUrl::default());
 
-        assert!(l);
-        assert!(s);
+            assert!(l);
+            assert!(s);
 
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "global");
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "global");
 
-        player.state_machine_post_event(&Event::PointerDown { x: 15.0, y: 45.0 });
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_1");
+            let star_1_box = player.get_layer_bounds("star_1");
 
-        player.state_machine_post_event(&Event::PointerDown { x: 30.0, y: 45.0 });
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_2");
+            player.state_machine_post_event(&Event::PointerDown {
+                x: star_1_box[0],
+                y: star_1_box[1],
+            });
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_1");
 
-        player.state_machine_post_event(&Event::PointerDown { x: 45.0, y: 45.0 });
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_3");
+            let star_2_box = player.get_layer_bounds("star_2");
 
-        player.state_machine_post_event(&Event::PointerDown { x: 60.0, y: 45.0 });
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_4");
+            player.state_machine_post_event(&Event::PointerDown {
+                x: star_2_box[0],
+                y: star_2_box[1],
+            });
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_2");
 
-        player.state_machine_post_event(&Event::PointerDown { x: 75.0, y: 45.0 });
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_5");
+            let star_3_box = player.get_layer_bounds("star_3");
+            player.state_machine_post_event(&Event::PointerDown {
+                x: star_3_box[0],
+                y: star_3_box[1],
+            });
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_3");
 
-        // Test that pointerUp anywhere on the canvas sets us back to global
-        player.state_machine_post_event(&Event::PointerUp { x: 0.0, y: 0.0 });
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_6");
+            let star_4_box = player.get_layer_bounds("star_4");
+            player.state_machine_post_event(&Event::PointerDown {
+                x: star_4_box[0],
+                y: star_4_box[1],
+            });
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_4");
+
+            let star_5_box = player.get_layer_bounds("star_5");
+            player.state_machine_post_event(&Event::PointerDown {
+                x: star_5_box[0],
+                y: star_5_box[1],
+            });
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_5");
+
+            let star_6_box = player.get_layer_bounds("star_6");
+            // Test that pointerUp anywhere on the canvas sets us back to global
+            player.state_machine_post_event(&Event::PointerUp {
+                x: star_6_box[0],
+                y: star_6_box[1],
+            });
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_6");
+        }
     }
 
     #[test]
@@ -105,148 +134,160 @@ mod tests {
 
     #[test]
     pub fn pointer_enter_test() {
-        let global_state =
-            include_str!("fixtures/statemachines/interaction_tests/pointer_enter.json");
-        let player = DotLottiePlayer::new(Config::default());
-        player.load_dotlottie_data(include_bytes!("fixtures/star_marked.lottie"), 100, 100);
-        let l = player.state_machine_load_data(global_state);
-        let s = player.state_machine_start(OpenUrl::default());
+        #[cfg(feature = "thorvg-v1")]
+        {
+            let global_state =
+                include_str!("fixtures/statemachines/interaction_tests/pointer_enter.json");
+            let player = DotLottiePlayer::new(Config::default());
+            player.load_dotlottie_data(include_bytes!("fixtures/star_marked.lottie"), 100, 100);
+            let l = player.state_machine_load_data(global_state);
+            let s = player.state_machine_start(OpenUrl::default());
 
-        assert!(l);
-        assert!(s);
+            assert!(l);
+            assert!(s);
 
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_0");
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_0");
 
-        player.state_machine_post_event(&Event::PointerEnter { x: 15.0, y: 45.0 });
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_1");
+            player.state_machine_post_event(&Event::PointerEnter { x: 15.0, y: 45.0 });
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_1");
 
-        player.state_machine_post_event(&Event::PointerEnter { x: 30.0, y: 45.0 });
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_2");
+            player.state_machine_post_event(&Event::PointerEnter { x: 30.0, y: 45.0 });
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_2");
 
-        player.state_machine_post_event(&Event::PointerEnter { x: 45.0, y: 45.0 });
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_3");
+            player.state_machine_post_event(&Event::PointerEnter { x: 45.0, y: 45.0 });
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_3");
 
-        player.state_machine_post_event(&Event::PointerEnter { x: 60.0, y: 45.0 });
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_4");
+            player.state_machine_post_event(&Event::PointerEnter { x: 60.0, y: 45.0 });
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_4");
 
-        player.state_machine_post_event(&Event::PointerEnter { x: 75.0, y: 45.0 });
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_5");
+            player.state_machine_post_event(&Event::PointerEnter { x: 75.0, y: 45.0 });
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_5");
+        }
     }
 
     #[test]
     pub fn pointer_enter_via_move_test() {
-        let global_state =
-            include_str!("fixtures/statemachines/interaction_tests/pointer_enter.json");
-        let player = DotLottiePlayer::new(Config::default());
-        player.load_dotlottie_data(include_bytes!("fixtures/star_marked.lottie"), 100, 100);
-        let l = player.state_machine_load_data(global_state);
-        let s = player.state_machine_start(OpenUrl::default());
+        #[cfg(feature = "thorvg-v1")]
+        {
+            let global_state =
+                include_str!("fixtures/statemachines/interaction_tests/pointer_enter.json");
+            let player = DotLottiePlayer::new(Config::default());
+            player.load_dotlottie_data(include_bytes!("fixtures/star_marked.lottie"), 100, 100);
+            let l = player.state_machine_load_data(global_state);
+            let s = player.state_machine_start(OpenUrl::default());
 
-        assert!(l);
-        assert!(s);
+            assert!(l);
+            assert!(s);
 
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_0");
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_0");
 
-        player.state_machine_post_event(&Event::PointerMove { x: 15.0, y: 45.0 });
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_1");
+            player.state_machine_post_event(&Event::PointerMove { x: 15.0, y: 45.0 });
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_1");
 
-        player.state_machine_post_event(&Event::PointerMove { x: 30.0, y: 45.0 });
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_2");
+            player.state_machine_post_event(&Event::PointerMove { x: 30.0, y: 45.0 });
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_2");
 
-        player.state_machine_post_event(&Event::PointerMove { x: 45.0, y: 45.0 });
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_3");
+            player.state_machine_post_event(&Event::PointerMove { x: 45.0, y: 45.0 });
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_3");
 
-        player.state_machine_post_event(&Event::PointerMove { x: 60.0, y: 45.0 });
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_4");
+            player.state_machine_post_event(&Event::PointerMove { x: 60.0, y: 45.0 });
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_4");
 
-        player.state_machine_post_event(&Event::PointerMove { x: 75.0, y: 45.0 });
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_5");
+            player.state_machine_post_event(&Event::PointerMove { x: 75.0, y: 45.0 });
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_5");
+        }
     }
 
     #[test]
     pub fn pointer_exit_test() {
-        let global_state =
-            include_str!("fixtures/statemachines/interaction_tests/pointer_exit.json");
-        let player = DotLottiePlayer::new(Config::default());
-        player.load_dotlottie_data(include_bytes!("fixtures/star_marked.lottie"), 100, 100);
-        let l = player.state_machine_load_data(global_state);
-        let s = player.state_machine_start(OpenUrl::default());
+        #[cfg(feature = "thorvg-v1")]
+        {
+            let global_state =
+                include_str!("fixtures/statemachines/interaction_tests/pointer_exit.json");
+            let player = DotLottiePlayer::new(Config::default());
+            player.load_dotlottie_data(include_bytes!("fixtures/star_marked.lottie"), 100, 100);
+            let l = player.state_machine_load_data(global_state);
+            let s = player.state_machine_start(OpenUrl::default());
 
-        assert!(l);
-        assert!(s);
+            assert!(l);
+            assert!(s);
 
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_0");
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_0");
 
-        player.state_machine_post_event(&Event::PointerEnter { x: 15.0, y: 45.0 });
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_1");
-        player.state_machine_post_event(&Event::PointerExit { x: 0.0, y: 0.0 });
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_0");
+            player.state_machine_post_event(&Event::PointerEnter { x: 15.0, y: 45.0 });
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_1");
+            player.state_machine_post_event(&Event::PointerExit { x: 0.0, y: 0.0 });
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_0");
+        }
     }
 
     #[test]
     pub fn pointer_exit_via_move_test() {
-        let global_state =
-            include_str!("fixtures/statemachines/interaction_tests/pointer_exit.json");
-        let player = DotLottiePlayer::new(Config::default());
-        player.load_dotlottie_data(include_bytes!("fixtures/star_marked.lottie"), 100, 100);
-        let l = player.state_machine_load_data(global_state);
-        let s = player.state_machine_start(OpenUrl::default());
+        #[cfg(feature = "thorvg-v1")]
+        {
+            let global_state =
+                include_str!("fixtures/statemachines/interaction_tests/pointer_exit.json");
+            let player = DotLottiePlayer::new(Config::default());
+            player.load_dotlottie_data(include_bytes!("fixtures/star_marked.lottie"), 100, 100);
+            let l = player.state_machine_load_data(global_state);
+            let s = player.state_machine_start(OpenUrl::default());
 
-        assert!(l);
-        assert!(s);
+            assert!(l);
+            assert!(s);
 
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_0");
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_0");
 
-        player.state_machine_post_event(&Event::PointerMove { x: 15.0, y: 45.0 });
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_1");
-        player.state_machine_post_event(&Event::PointerMove { x: 0.0, y: 0.0 });
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_0");
+            player.state_machine_post_event(&Event::PointerMove { x: 15.0, y: 45.0 });
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_1");
+            player.state_machine_post_event(&Event::PointerMove { x: 0.0, y: 0.0 });
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_0");
 
-        player.state_machine_post_event(&Event::PointerMove { x: 30.0, y: 45.0 });
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_2");
-        player.state_machine_post_event(&Event::PointerMove { x: 0.0, y: 0.0 });
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_0");
+            player.state_machine_post_event(&Event::PointerMove { x: 30.0, y: 45.0 });
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_2");
+            player.state_machine_post_event(&Event::PointerMove { x: 0.0, y: 0.0 });
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_0");
 
-        player.state_machine_post_event(&Event::PointerMove { x: 45.0, y: 45.0 });
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_3");
-        player.state_machine_post_event(&Event::PointerMove { x: 0.0, y: 0.0 });
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_0");
+            player.state_machine_post_event(&Event::PointerMove { x: 45.0, y: 45.0 });
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_3");
+            player.state_machine_post_event(&Event::PointerMove { x: 0.0, y: 0.0 });
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_0");
 
-        player.state_machine_post_event(&Event::PointerMove { x: 60.0, y: 45.0 });
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_4");
-        player.state_machine_post_event(&Event::PointerMove { x: 0.0, y: 0.0 });
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_0");
+            player.state_machine_post_event(&Event::PointerMove { x: 60.0, y: 45.0 });
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_4");
+            player.state_machine_post_event(&Event::PointerMove { x: 0.0, y: 0.0 });
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_0");
 
-        player.state_machine_post_event(&Event::PointerMove { x: 75.0, y: 45.0 });
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_5");
-        player.state_machine_post_event(&Event::PointerMove { x: 0.0, y: 0.0 });
-        let curr_state_name = get_current_state_name(&player);
-        assert_eq!(curr_state_name, "star_0");
+            player.state_machine_post_event(&Event::PointerMove { x: 75.0, y: 45.0 });
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_5");
+            player.state_machine_post_event(&Event::PointerMove { x: 0.0, y: 0.0 });
+            let curr_state_name = get_current_state_name(&player);
+            assert_eq!(curr_state_name, "star_0");
+        }
     }
 
     #[test]
